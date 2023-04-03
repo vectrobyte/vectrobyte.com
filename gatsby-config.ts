00a3114b64cb0def3cc854663267d1b14073c5a4
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-03 21:55:58 +0530

    chore: add new font using gatsby omni font loader

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -7,7 +7,20 @@ const config: GatsbyConfig = {
   },
   plugins: [
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;900&display=swap`,
+          },
+        ],
+      },
+    },
   ],
-}
+};
 
-export default config
+export default config;
