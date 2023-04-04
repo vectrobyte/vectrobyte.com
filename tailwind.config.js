2022c6576688cfedcc0164860dd570933dd8123a
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-04 12:03:39 +0530

    feat: design a basic layout of the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#685BA5',
+        primary: '#2B2F32',
+        secondary: '#23272A',
+        light: '#FAF9F6',
+        muted: '#9E9E9E',
       },
     },
   },
