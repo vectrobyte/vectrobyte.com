6ae7e01aa20542436feb55f54b8cb30e36344f6b
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-04 22:54:21 +0530

    chore: design a basic layout of index page with navbar and hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 
-// markup
+import Hero from '../components/Hero';
+import Layout from '../layouts';
+
 const IndexPage = () => {
   return (
-    <main className="min-h-screen bg-white dark:bg-secondary">
-      <section aria-label="hero" className="bg-light dark:bg-primary">
-        <div className="container mx-auto flex items-center justify-between">
-          <div>
-            <div aria-label="navbar" className="py-10 flex items-center">
-              <h1 className="text-4xl dark:text-white">
-                <span className="font-semibold">Dilip</span>{' '}
-                <span className="font-thin">Bhattarai</span>
-              </h1>
-            </div>
-
-            <div className="py-52">
-              <div className="">
-                <h1 className="text-5xl mb-8 text-gray-700 dark:text-gray-400">Hi!</h1>
-                <h1 className="text-6xl mb-4 text-gray-800 dark:text-white">
-                  {' '}
-                  I am Dilip Bhattarai
-                </h1>
-                <h5 className="text-lg font-thin uppercase mb-8 dark:text-muted">
-                  Full-Stack Web Developer
-                </h5>
-                <button className="bg-primary dark:bg-light text-white dark:text-primary rounded-full text-base py-3 px-5">
-                  Let&apos;s Chat
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div></div>
-        </div>
-      </section>
-    </main>
+    <Layout>
+      <Hero />
+    </Layout>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // enable dark mode
   theme: {
     extend: {
       colors: {
         primary: '#2B2F32',
         secondary: '#23272A',
-        light: '#FAF9F6',
-        muted: '#9E9E9E',
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
