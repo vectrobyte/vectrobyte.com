a54b22032db7661f631c3866ede1236c5d36a1da
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-11 11:54:09 +0530

    feat: configure request with axios and pass validation errors into the contact form

--- src/components/toasts/SuccessToast/SuccessToast.tsx
+++ src/components/toasts/SuccessToast/SuccessToast.tsx
@@ -3,7 +3,7 @@ import { AiFillCheckCircle } from 'react-icons/ai';
 
 const SuccessToast: React.FC = ({ children }) => {
   return (
-    <div className="text-gray-600 flex items-start">
+    <div className="text-primary dark:text-gray-100 flex items-start">
       <AiFillCheckCircle className="mr-4 text-2xl flex-shrink-0" />
       {children}
     </div>
