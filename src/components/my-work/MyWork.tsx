203086c3c7444ac4be527b10c8a5bc6e3ceaa18e
Dilip Bhattarai <vectrobyte@gmail.com>
2023-04-16 23:49:21 +0530

    chore: move my work section into directory

--- src/components/my-work/MyWork.tsx
+++ src/components/my-work/MyWork.tsx
@@ -4,7 +4,7 @@ type MyProjectsProps = React.HTMLAttributes<HTMLElement>;
 
 const MyWork: React.FC<MyProjectsProps> = () => {
   return (
-    <section id="my-projects" className="w-full h-full py-16 lg:py-32 transition-colors">
+    <section id="my-work" className="w-full h-full py-16 lg:py-32 transition-colors">
       <div className="container mx-auto">
         <div className="text-center">
           <h1 className="text-4xl lg:text-5xl text-gray-800 dark:text-white transition-colors md:mb-6">
